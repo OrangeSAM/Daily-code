2f4c1359ca6168878fcb7f96b4573e93f31313fb
sam <amazing_sam@outlook.com>
2018-09-21 17:37:59 +0800

    end of the afternoon

--- website/Js/index.js
+++ website/Js/index.js
@@ -2,7 +2,7 @@ var tag=["矜持是什么","性生活","性爱","性虐狂","挽回爱情","分�
         "婚姻经营","放弃婚姻","离婚原因","女神计划","夫妻",
         "男女相处","情感维系","结婚"];
 var rtag=document.getElementsByClassName('tags');
-window.onload=function randomtag(){
+function randomtag(){
     rtag[0].innerHTML=tag[Math.floor(Math.random()*tag.length)];
     rtag[1].innerHTML=tag[Math.floor(Math.random()*tag.length)];
     rtag[2].innerHTML=tag[Math.floor(Math.random()*tag.length)];
@@ -18,3 +18,6 @@ window.onload=function randomtag(){
     rtag[12].innerHTML=tag[Math.floor(Math.random()*tag.length)];
     rtag[13].innerHTML=tag[Math.floor(Math.random()*tag.length)];
 }
+var btn=document.getElementsByClassName('randomtag');
+debugger;
+btn.click=randomtag();
