1fa6a261de142f186c88fca8d08bcad9a2eec6d8
sam <amazing_sam@outlook.com>
2019-04-27 01:58:06 +0800

    geektime vue learning

---
+++ geekTime/vue/05the-first-component/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <todoitem/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import ToDoItem from "./components/ToDoItem";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    ToDoItem
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ geekTime/vue/05the-first-component/src/components/ToDoItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li class="item">{{item}}</li>
+</template>
+
+<script>
+export default {
+  name: "todoitem"
+};
+</script>
+
+
+<style scoped>
+</style>
