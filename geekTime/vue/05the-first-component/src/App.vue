fae2ce37e327b0fc60f0779c82890655bab8ebf8
sam <amazing_sam@outlook.com>
2019-05-11 14:26:24 +0800

    傻逼学校

--- geekTime/vue/05the-first-component/src/App.vue
+++ geekTime/vue/05the-first-component/src/App.vue
@@ -1,19 +1,53 @@
 <template>
   <div id="app">
+    <div>{{msg}}</div>
     <img src="./assets/logo.png">
-    <todoitem/>
+    <div>
+      <input type="text" v-model="info">
+      <button @click="handleClick">添加</button>
+    </div>
+    <!-- <ToDoItem v-for="(item, index) in list" :key="index" :item="item"></ToDoItem> -->
+    <!-- 插槽的方式实现 -->
+    <!-- 插槽和props对于值传递的方式是不一样的 -->
+    <ToDoItem v-for="(item, index) in list" :key="index">
+      <!-- 2.5slot语法 -->
+      <!-- <span slot="item" style="font-size:20px">{{item}}</span> -->
+      <!-- 2.6slot语法 -->
+      <template v-slot:item="itemProps">
+        <span :style="{fontSize: '20px', color: itemProps.checked ? 'red' : 'blue' }">{{item}}</span>
+      </template>
+      <!-- 有两个的时候只渲染一个 有点奇怪 -->
+    </ToDoItem>
+
+    <!-- 事件demo -->
+    <Event></Event>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
 import ToDoItem from "./components/ToDoItem";
+import Event from "./components/Event";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
-    ToDoItem
+    ToDoItem,
+    Event
+  },
+  data() {
+    return {
+      msg: "hello sam",
+      info: "",
+      list: []
+    };
+  },
+  methods: {
+    handleClick() {
+      this.list.push(this.info);
+      this.info = "";
+    }
   }
 };
 </script>
--- geekTime/vue/05the-first-component/src/components/ToDoItem.vue
+++ geekTime/vue/05the-first-component/src/components/ToDoItem.vue
@@ -1,13 +1,31 @@
 <template>
-  <li class="item">{{item}}</li>
+  <!-- <li class="item">{{item}}</li> -->
+  <li class="item">
+    <input type="checkbox" name id v-model="checked">
+    <slot name="item" v-bind="{checked}"></slot>
+  </li>
 </template>
 
 <script>
 export default {
-  name: "todoitem"
+  name: "todoitem",
+  props: {
+    item: {
+      type: String,
+      default: "helloSam"
+    }
+  },
+  data() {
+    return {
+      checked: null
+    };
+  }
 };
 </script>
 
 
 <style scoped>
+.item {
+  color: lightcoral;
+}
 </style>
