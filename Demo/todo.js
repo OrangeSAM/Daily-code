68fc75572dc903d1263631c52fd15b9c2cd6eb30
Sam <amazing_sam@outlook.com>
2019-09-19 00:16:39 +0800

    all features almost complished

--- Demo/todo.js
+++ Demo/todo.js
@@ -11,34 +11,78 @@ let fs = require("fs");
 
 // 获取命令行中除前面两项路径外的参数
 let argus = process.argv.slice(2);
-
 // 获取命令行的动作
 const action = argus[0];
+const content = argus[1];
+const content1 = argus[2];
+let readContent;
+let taskList = [];
+
+fs.stat("F:\\Code\\Daily-code\\Demo\\todoDb", function(err, stat) {
+  if (err === null) {
+    // 存在则获取数据库中的内容
+    readContent = fs.readFileSync(
+      "F:\\Code\\Daily-code\\Demo\\todoDb",
+      "utf-8"
+    );
+    taskList = JSON.parse(readContent);
+
+    if (action === "add") {
+      taskList.push([content, false]);
+    }
+    if (action === "list") {
+      console.log(taskList);
+    }
+    if (action === "delete") {
+      taskList.splice(content - 1, 1);
+    }
+    if (action === "done") {
+      taskList[content - 1][1] = true;
+    }
+    if (action === "edit") {
+      taskList[content - 1][0] = content1;
+    }
+    fs.writeFileSync(
+      "F:\\Code\\Daily-code\\Demo\\todoDb",
+      JSON.stringify(taskList)
+    );
+  } else if (err.code === "ENOENT") {
+    // 不存在则新建立数据库
+    // fs.writeFileSync("F:\\Code\\Daily-code\\Demo\\todoDb", "");
+    if (action === "add") {
+      taskList.push([content, false]);
+      fs.writeFileSync(
+        "F:\\Code\\Daily-code\\Demo\\todoDb",
+        JSON.stringify(taskList)
+      );
+    }
+  } else {
+    console.log("oops, something wrong, please try again");
+  }
+});
 
 // 获取数据库中的内容
-let readContent = fs.readFileSync(
-  "F:\\Code\\Daily-code\\Demo\\todoDb",
-  "utf-8"
-);
+// let readContent = fs.readFileSync(
+//   "F:\\Code\\Daily-code\\Demo\\todoDb",
+//   "utf-8"
+// );
 // 如果不使用第二个参数即编码格式，可以通过对readContent toString获得，不然就是buffer格式。
 // console.log(readContent);
 
-// 用一个数组来保存添加的任务
-let taskList = JSON.parse(readContent);
+// 用一个数组来保存添加的任务，将字符串数组转为数组
+// let taskList = JSON.parse(readContent);
 
-if (action === "add") {
-  taskList.push(argus[1]);
-  // 直接存入貌似会有编码上的问题
-  // fs.writeFileSync("F:\\Code\\Daily-code\\Demo\\todoDb", argus[1]);
-  fs.writeFileSync(
-    "F:\\Code\\Daily-code\\Demo\\todoDb",
-    JSON.stringify(taskList)
-  );
-  // 序列化，变成数组的样子
-}
-if (action === "delete") {
-}
-if (action !== "add" && argus[0] !== "delete") {
-  console.log("oops, it seems the action is wrong");
-}
-console.log(taskList);
+// if (action === "add") {
+//   taskList.push(argus[1]);
+// 直接存入貌似会有编码上的问题
+// fs.writeFileSync("F:\\Code\\Daily-code\\Demo\\todoDb", argus[1]);
+//   fs.writeFileSync(
+// "F:\\Code\\Daily-code\\Demo\\todoDb",
+// JSON.stringify(taskList)
+//   ); // 序列化，变成数组的样子
+// }
+// if (action === "delete") {
+// }
+// if (action !== "add" && argus[0] !== "delete") {
+//   console.log("oops, it seems the action is wrong");
+// }
