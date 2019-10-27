32504016555f97053d102553d1afd54241ec013b
Sam <amazing_sam@outlook.com>
2019-10-27 20:39:20 +0800

    整理代码

--- Demo/judgeTimes.js
+++ Demo/judgeTimes.js
@@ -1,20 +1,19 @@
-var str="asdfgss";
-var json={};
-for(var i=0;i<str.length;i++){
+var str = "asdfgss";
+var json = {};
+for (var i = 0; i < str.length; i++) {
     //判断json这个对象中是否存在这个key，存在则value+1
-    if(!json[str.charAt(i)]){
-        debugger;
-        json[str.charAt(i)]=1;
-    }else{
+    if (!json[str.charAt(i)]) {
+        json[str.charAt(i)] = 1;
+    } else {
         json[str.charAt(i)]++;
     }
 }
-var iMax=0;
-var iIndex='';
-for(var i in json){
-    if (json[i]>iMax){
-        iMax=json[i];
-        iIndex=i;
+var iMax = 0;
+var iIndex = '';
+for (var i in json) {
+    if (json[i] > iMax) {
+        iMax = json[i];
+        iIndex = i;
     }
 }
-console.log('出现次数最多的是：'+iIndex+'出现'+iMax+'次');
+console.log('出现次数最多的是：' + iIndex + '出现' + iMax + '次');
