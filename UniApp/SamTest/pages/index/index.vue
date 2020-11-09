4ca3966e9f9c681cdf4075d4531f8ec669168633
SamLiu <amazing_sam@outlook.com>
2020-11-09 20:30:22 +0800

    feat: 修改样式，写todo

--- UniApp/SamTest/pages/index/index.vue
+++ UniApp/SamTest/pages/index/index.vue
@@ -1,9 +1,12 @@
 <template>
 	<view class="card-wrapper">
-		<view class="card-title">{{lunarData.monthCn}}{{lunarData.dayCn}} {{lunarData.gzYear}}年 {{lunarData.animal}}年 {{lunarData.gzMonth}}月 {{lunarData.gzDay}}日</view>
+		<view class="card-title">
+				{{lunarData.animal}}年 {{lunarData.monthCn}} {{lunarData.dayCn}} 
+				{{lunarData.gzYear}}年 {{lunarData.gzMonth}}月 {{lunarData.gzDay}}日
+		</view>
 		<view class="card-date">
-			<view class="date-month">十月 OCTOBER</view>
-			<view class="date-detail">21</view>
+			<view class="date-month">{{monthMap[formatedDate.month].cn}} {{monthMap[formatedDate.month].en}} </view>
+			<view class="date-detail">{{formatedDate.day}}</view>
 		</view>
 		<view class="card-content">
 			<view class="content-detail">
@@ -11,11 +14,22 @@
 			</view>
 			<view class="content-intro">
 				<image src="../../static/qrcode.png" mode=""></image>
-				<view class="tips">秦小明</view>
 			</view>
 		</view>
 	</view>
 </template>
+
+<!-- todo -->
+<!--
+1. 如何分享到微信朋友圈
+   观察到目前已有的实现无法直接一键转发朋友圈，都是保存本地手动发布
+2. 如何分享给微信好友
+   应该有API，不知道要不要登录
+3. 如何保存图片到本地 
+   直接抄已有的解决方案
+4. 字体
+5. 通过字数来判断字体大小
+-->
 
 <script>
 	import solarLunar from 'solarlunar'
@@ -24,7 +38,57 @@
 			return {
 				formatedDate: {},
 				// 转换为农历后的数据
-				lunarData: {}
+				lunarData: {},
+				monthMap: {
+					1: {
+						cn: '正月',
+						en: 'January'
+					},
+					2: {
+						cn: '二月',
+						en: 'January'
+					},
+					3: {
+						cn: '三月',
+						en: 'January'
+					},
+					4: {
+						cn: '四月',
+						en: 'January'
+					},
+					5: {
+						cn: '五月',
+						en: 'January'
+					},
+					6: {
+						cn: '六月',
+						en: 'January'
+					},
+					7: {
+						cn: '七月',
+						en: 'January'
+					},
+					8: {
+						cn: '八月',
+						en: 'January'
+					},
+					9: {
+						cn: '九月',
+						en: 'January'
+					},
+					10: {
+						cn: '十月',
+						en: 'January'
+					},
+					11: {
+						cn: '冬月',
+						en: 'January'
+					},
+					12: {
+						cn: '腊月',
+						en: 'January'
+					},
+				}
 			}
 		},
 		computed: {
@@ -43,7 +107,6 @@
 			this.todayDate()
 			console.log(this.formatedDate)
 			this.lunarData = solarLunar.solar2lunar(this.formatedDate.year, this.formatedDate.month, this.formatedDate.day)
-			console.log(this.lunarData)
 		},
 	}
 </script>
@@ -56,11 +119,12 @@
 		font-family: 'Times New Roman', Times, serif;
 	}
 	.card-title {
-		line-height: 80rpx;
-		height: 80rpx;
+		line-height: 76rpx;
+		height: 76rpx;
 		text-align: center;
 		font-weight: 600;
-		font-family: cursive;
+		font-size: 16px;
+		font-family: 'fangsong';
 		border-bottom: 1px solid #333333;
 	}
 	.card-date {
@@ -68,6 +132,7 @@
 	} 
 	.card-date .date-month {
 		margin: 30rpx;
+		font-size: 20px;
 		font-weight: 800;
 	}
 	.card-date .date-detail {
@@ -99,14 +164,4 @@
 		font-family: 'fangsong';
 		border-right: 1px solid #f1f1f1;;
 	}
-	// 遮罩区样式, 作废
-	/* .card-content .cover {
-		position: absolute;
-		z-index: 2;
-		top: 15px;
-		width: 50px;
-		height: 50px;
-		background-color: white;
-		opacity: 0.5;
-	} */
 </style>
